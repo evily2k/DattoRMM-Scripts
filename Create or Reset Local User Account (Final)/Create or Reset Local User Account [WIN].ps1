78fb22c80ea00f12097b236c653be33d128f8630
Dan Meddock <dmeddock@kandeconsulting.com>
2022-04-25 09:44:24 -0500

    Create or Reset Local User Account

--- Create or Reset Local User Account (Final)/Create or Reset Local User Account [WIN].ps1
+++ Create or Reset Local User Account (Final)/Create or Reset Local User Account [WIN].ps1
@@ -14,18 +14,23 @@ $localPassword = $env:localPassword
 
 # Main
 try{	
+	# Checks if a value was enter in the username field in the DattoRMM Script; if not exit.
 	If(!($localAccount)){Write-Host "No username was specified. Exiting..."; Exit 1}
 	If(!($localPassword)){Write-Host "No password was specified. Exiting..."; Exit 1}
 
+	# Get local user information
 	$userCheck = get-localuser | Where-Object {$_.Name -eq $localAccount}
 	$administratorsAccount = Get-WmiObject Win32_Group -filter "LocalAccount=True AND SID='S-1-5-32-544'" 
 	$administratorQuery = "GroupComponent = `"Win32_Group.Domain='" + $administratorsAccount.Domain + "',NAME='" + $administratorsAccount.Name + "'`"" 
 	$user = Get-WmiObject Win32_GroupUser -filter $administratorQuery | select PartComponent |where {$_ -match $localAccount} 
 	
+	# Check if user exists
 	If($userCheck){
+		# Check if password reset checkbox is checked
 		If($PasswordReset){
 			Write-Host "$localAccount already exists. Resetting $localAccount password to $localPassword."
 			net user $localAccount $localPassword /expires:never
+			#if user isnt an admin add them to the Administrators group
 			If(!($user)){
 				Write-Host "Adding $localAccount to local administrators group."
 				net localgroup administrators $localAccount /add
@@ -37,10 +42,12 @@ try{
 			Exit 1
 			}
 	}Else{
+		# If user doesnt exist then create the account
 		Write-Host "Creating $localAccount account with the password $localPassword..."
 		net user $localAccount $localPassword /add
 		Write-Host "Setting $localAccount password to never expire."
 		net user $localAccount $localPassword /expires:never
+		# Add user to admin group if admin check box is checked
 		If($admin){
 			Write-Host "Adding $localAccount to the Administrators group."
 			net localgroup administrators $localAccount /add
