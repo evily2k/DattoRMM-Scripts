46fd9845f7526911de8ebe1e27d40568e01580a9
Dan Meddock <snoman014@gmail.com>
2022-12-30 10:29:08 -0600

    Updates

---
+++ SilentInstallHQ Deployment Script/LibreOffice Deployment [SIHQ].ps1
@@ -0,0 +1,62 @@
+<#
+TITLE: LibreOffice [WIN]
+PURPOSE: Script to install or uninstall LibreOffice (SilentUnstallHQ Template)
+CREATOR: Dan Meddock
+CREATED: 30DEC2022
+LAST UPDATED: 30DEC2022
+#>
+
+# Declarations
+$workingDir = "C:\KEworking"
+$appZip = 'LibreOffice.zip'
+$installPackage = "Deploy-LibreOffice.ps1"
+$appName = "LibreOffice"
+
+# Paths and Variable manipulation (dont change)
+$appZipPath = join-path -path $workingDir -childpath $appZip
+$appPath = $appZipPath -replace ".zip",""
+$deploymentTool = join-path -path $appPath -childpath $installPackage
+
+# Commands
+$install = {Powershell.exe -ExecutionPolicy Bypass $deploymentTool -DeploymentType "Install" -DeployMode "silent"}
+$uninstall = {Powershell.exe -ExecutionPolicy Bypass $deploymentTool -DeploymentType "Uninstall" -DeployMode "silent"}
+
+# Function to remove instal files once the install/uninstall is complete
+function removeInstaller {
+	Write-Output "Removing installer files."
+	Remove-Item -path $appZipPath -recurse -force
+	Remove-Item -path $appPath -recurse -force
+}
+
+# Main
+Try{
+	# Check if working directory exists
+	If(!(test-path $workingDir -PathType Leaf)){new-item $workingDir -ItemType Directory -force}
+	
+	# Transfer installers to computer
+	Write-Host "Transferring $appName deployment tool to device."
+	Copy-Item $appZip -Destination $workingDir -force
+	
+	# Extracting zip file contents
+	Write-Host "Extracting $appName deployment tool."
+	Expand-Archive -LiteralPath $appZipPath -DestinationPath $workingDir -Force
+	
+	# Check if variable is set to install or uninstall
+	If($env:installApp -eq "True"){
+		# Start Lenovo System Update install
+		Write-Host "Starting install of $appName."
+		& $install
+		removeInstaller
+		Exit 0
+	}Else{
+		# Start Lenovo System Update uninstall
+		Write-Host "Starting uninstall of $appName."
+		& $uninstall
+		removeInstaller
+		Exit 0
+	}
+}Catch{
+	# Catch any errors thrown and exit with an error
+	Write-Error $_.Exception.Message 
+	Exit 1
+}
