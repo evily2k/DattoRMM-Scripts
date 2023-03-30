f5966e051f2dc80f4ba19decc2e44db2b07363a8
Dan Meddock <snoman014@gmail.com>
2023-03-30 14:14:45 -0500

    Update Dell Command - Install and Run Updates - Force Reboot [WIN].ps1
    Updated how DCU is installed since downloading from dells site is failing. Used Chocolatey repos instead.

--- Dell Command - Install and Run Updates (Final)/Dell Command - Install and Run Updates - Force Reboot [WIN].ps1
+++ Dell Command - Install and Run Updates (Final)/Dell Command - Install and Run Updates - Force Reboot [WIN].ps1
@@ -1,53 +1,102 @@
 <#
 TITLE: Dell Command - Install and Ran Updates - Force Reboot[WIN]
-PURPOSE: Installs Dell Command, runs scan for updates, installs updates, if a reboot is required it will reboot after the installation completes, suspends bitlocker
+PURPOSE: Installs Dell Command vias chocolatey, runs scan for updates, suspends bitlocker, installs updates, if a reboot is required it will reboot after the installation completes.
 CREATOR: Dan Meddock
-CREATED: 09DEC2021
-LAST UPDATED: 25APR2022
+CREATED: 10DEC2021
+LAST UPDATED: 23MAR2023
 #>
 
-# Function to output log results to Datto's activity log
-Function writeDattoActivity(){
-	$getLog = @(get-content -path $LogFilePath)
-	foreach ($message in $getLog){write-host $message}
+# Log Get-ChocoApps output to log file
+Start-Transcript -Path "C:\temp\installUpdateDCU.log"
+
+# Declarations
+# Add any Chocolatey supported applications to this list to be installed
+$app = 'dellcommandupdate'
+
+# Enable script execution
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Download and install Chocolatey
+function InstallChoco {
+    $bool = 0
+    Try {
+        if (!(Test-Path($env:ChocolateyInstall + "\choco.exe"))) {
+			iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+        }
+    }Catch{
+        Write-Host $($_.Exception.Message)
+    }
 }
 
-# Main
-If ((Get-ComputerInfo).CsManufacturer -match "Dell"){
-	try {
-		# Declarations
-		$DownloadURL = "https://downloads.dell.com/FOLDER07820512M/1/Dell-Command-Update-Application_8DGG4_WIN_4.4.0_A00.EXE"
-		$DownloadLocation = "C:\Temp\Dell"
-		$LogFilePath = "$($DownloadLocation)\dellUpdate.log"
+# Installs the application using Chocolatey
+function InstallDCU {
+	Try{
+		if ($app -ne " ") {
+			$app.Split(" ") | % {
+				if (!($_ -like " ") -and $_.length -ne 0) {
+					Write-Host Installing $_ 
+					& cmd /c """$env:ChocolateyInstall\choco.exe"" install $_ -y"
+				}
+			}
+		} else {
+			$VersionChoco = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($env:ChocolateyInstall + "\choco.exe").ProductVersion
+			Write-Host Installing
+			Write-Host Chocolatey v$VersionChoco
+			Write-Host Package name is required. Please pass at least one package name to install.`n
+		}
+	}Catch{
+		Write-Host $($_.Exception.Message)
+	}
+}
+
+# Function to run and install Dell Command updates
+Function updateDell {
+	# Locate dcu-cli.exe and start the Dell Command and Update process
+	Try{
+		# Download direcotry and DCU-CLI variables
+		$logFile = "C:\temp\dellUpdate.log"
 		$druLocation64 = "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe"
 		$druLocation32 = "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe"
 		
-		# Check if dell command exists; if not then download and install
-		$TestDownloadLocation = Test-Path $DownloadLocation
-		if (!$TestDownloadLocation) { new-item $DownloadLocation -ItemType Directory -force }
-		$TestDownloadLocationZip = Test-Path "$($DownloadLocation)\DellCommandUpdate.exe"
-		if (!$TestDownloadLocationZip) { 
-			(New-Object System.Net.WebClient).DownloadFile($DownloadURL, "$($DownloadLocation)\DellCommandUpdate.exe")
-			Start-Process -FilePath "$($DownloadLocation)\DellCommandUpdate.exe" -ArgumentList '/s' -Verbose -Wait
-			set-service -name 'DellClientManagementService' -StartupType Manual
-		} 
-	}
-	catch {
-		write-host "The download and installation of DCUCli failed. Error: $($_.Exception.Message)"
-	}
-	# Run Dell Command and update
-	try {	
-		if (test-path -path $druLocation32 -pathtype leaf){$druDir = $druLocation32}else{$druDir = $druLocation64}	
-		write-host "Starting Dell Command update."
-		start-process -NoNewWindow -FilePath $druDir -ArgumentList "/applyUpdates -silent -reboot=enable -autoSuspendBitLocker=enable -outputLog=$($DownloadLocation)\dellUpdate.log" -Wait
-	}catch{
+		if (($druLocation64) -or ($druLocation32)){
+			# Find dcu-cli.exe programfile location
+			if (test-path -path $druLocation32 -pathtype leaf){$druDir = $druLocation32}else{$druDir = $druLocation64}		
+			# Start Dell Command update process; apply all updates and ignore reboot; suspend bitlocker if detected and output log to C:\temp
+			write-host "Running Dell Command and Update to update dell drivers."
+			start-process -NoNewWindow $druDir -ArgumentList "/applyUpdates -silent -reboot=enable -autoSuspendBitLocker=enable -outputLog=$logFile" -Wait
+			get-content $logFile
+			sleep 5
+			Remove-Item $logFile -Force
+		}else{
+			# Dell Command Update was not found on this device
+			write-host "Dell Command Update is not installed on this computer."
+			Write-host "Skipping Dell Command Update."
+		}
+	}Catch{
+		# Catch any powershell errors and output the error message
 		write-host $_.Exception.Message
-		# Write log file output to console to display in Datto logging
-		writeDattoActivity
-		Exit 1
 	}
 }
 
-# Write log file output to console to display in Datto logging
-writeDattoActivity
-exit 0
+# Main
+
+# Enable TLS 1.2 security protocol
+try {
+	[Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072)
+} catch [system.exception] {
+	write-host "- ERROR: Could not implement TLS 1.2 Support."
+	write-host "  This can occur on Windows 7 devices lacking Service Pack 1."
+	write-host "  Please install that before proceeding."
+}
+
+# Start the Chocolatey install/update
+InstallChoco
+
+# Install Dell Command Update via Chocolatey
+InstallDCU
+
+# Run DCU and install updates and force a reboot if needed
+updateDell
+
+Stop-Transcript
+Exit 0
