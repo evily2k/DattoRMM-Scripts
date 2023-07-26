52f8eb4b2a361aa72963f3a7fdc8cb78215751ab
unknown <snoman014@gmail.com>
2023-07-26 01:22:18 -0500

    Updates

--- Disables WiFi and Bluetooth Adapters/Disables WiFi and Bluetooth Adapters.ps1
+++ Disables WiFi and Bluetooth Adapters/Disables WiFi and Bluetooth Adapters.ps1
@@ -2,9 +2,11 @@
 TITLE: Disables WiFi and Bluetooth Adapters [WIN]
 PURPOSE: Disables WiFi and Bluetooth but keeps Ethernet enabled
 CREATOR: Dan Meddock
-CREATED: 25JUNl023
-LAST UPDATED: 25JUN2023
+CREATED: 25JUl2023
+LAST UPDATED: 26JUl2023
 #>
+
+# Declarations
 
 # Function to disable network adapters except for Ethernet adapters
 function DisableNetworkAdaptersExceptEthernet {
@@ -13,7 +15,7 @@ function DisableNetworkAdaptersExceptEthernet {
     foreach ($adapter in $adapters) {
         if ($adapter.Status -eq 'Up') {
             Write-Host "Disabling network adapter $($adapter.Name)..."
-            #Disable-NetAdapter -Name $adapter.Name -Confirm:$false
+            Disable-NetAdapter -Name $adapter.Name -Confirm:$false
             Write-Host "Network adapter $($adapter.Name) disabled."
         }
     }
@@ -21,19 +23,26 @@ function DisableNetworkAdaptersExceptEthernet {
 
 # Function to disable Bluetooth
 function DisableBluetooth {
-    $bluetoothAdapters = Get-PnpDevice -Class Bluetooth
+    $bluetoothAdapters = Get-PnpDevice -Class Bluetooth -erroraction silentlycontinue
 
     foreach ($adapter in $bluetoothAdapters) {
         if ($adapter.Status -eq 'OK') {
             Write-Host "Disabling Bluetooth adapter $($adapter.Name)..."
-            #Disable-PnpDevice -InstanceId $adapter.InstanceId -Confirm:$false
+            Disable-PnpDevice -InstanceId $adapter.InstanceId -Confirm:$false
             Write-Host "Bluetooth adapter $($adapter.Name) disabled."
         }
     }
 }
 
-# Disable network adapters except Ethernet
-DisableNetworkAdaptersExceptEthernet
+# Main 
+Try{	
+	# Disable network adapters except Ethernet
+	DisableNetworkAdaptersExceptEthernet #-whatif
 
-# Disable Bluetooth
-DisableBluetooth
+	# Disable Bluetooth
+	DisableBluetooth #-whatif
+}catch{
+	# Catch any errors thrown and exit with an error
+	Write-Error $_.Exception.Message 
+}
+Exit 0
