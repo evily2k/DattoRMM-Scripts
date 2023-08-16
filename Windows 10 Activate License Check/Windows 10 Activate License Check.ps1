80bb5a9943dbba179aa9edebb7f3876808c2e6fb
Dan Medock <snoman014@gmail.com>
2023-08-15 23:59:25 -0500

    Updates

---
+++ Windows 10 Activate License Check/Windows 10 Activate License Check.ps1
@@ -0,0 +1,28 @@
+<#
+TITLE: Windows 10 Activate License Check [WIN]
+PURPOSE: Checks if Windows is permanently activated
+CREATOR: Dan Meddock
+CREATED: 15AUG2023
+LAST UPDATED: 15AUG2023
+#>
+
+# Declarations
+$testActivation = Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" | where { $_.PartialProductKey } | select Description, LicenseStatus
+
+# Main
+Try{
+	if($testActivation -eq $null){
+		Write-Host "Windows activation check failed to run. Please investigate."
+		#Exit 1
+	}
+	if ($testActivation -ne "1"){
+		Write-Host "Windows is not permanently activated."
+		#Exit 1
+	}else{
+		Write-Host "Windows is permanently activated."
+		#Exit 0
+	}
+}Catch{
+	# Catch any errors thrown and exit with an error
+	Write-Error $_.Exception.Message 
+}
